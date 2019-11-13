165c0d73a0bbbe63a9c3faf896cca36f5d249355
Int_21h <yuskovss@gmail.com>
2019-11-13 14:40:47 +0400

    feat: add style for header, logo, menu

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import TrendingGifs from './TrendingGifs';
 
 export default class MainLayout extends Component {
   render() {
     return (
       <div className="app">
-        <header className="header">          
-          <ul>
-            <li><Link to="tranding-gifs">Tranding Gifs</Link></li>
-            <li><Link to="search">Search</Link></li>
-            <li><Link to="sport">Sport</Link></li>
-            <li><Link to="stikers">Stikers</Link></li>
+        <header className="header">      
+          <div className="header__logo">
+            <h1>GIPHY</h1>
+          </div>
+          <ul className="menu header__menu">
+            <li className="menu__item"><Link to="/">Home</Link></li>
+            <li className="menu__item"><Link to="tranding-gifs">Tranding Gifs</Link></li>
+            <li className="menu__item"><Link to="search">Search</Link></li>
+            <li className="menu__item"><Link to="sport">Sport</Link></li>
+            <li className="menu__item"><Link to="stikers">Stikers</Link></li>
           </ul>
         </header>
         <main>          
