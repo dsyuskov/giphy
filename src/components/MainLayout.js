dad9b7264b998462ce3decada3c518bb617212c4
int <yuskovss@gmail.com>
2019-11-16 16:33:50 +0400

    feat: add correct search and navigation to links

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import { Link, withRouter, Route } from 'react-router-dom';
-import Search from '../components/Search';
-import SearchGifs from '../components/SearchGifs';
-import giphyService from '../services/giphy';
+import { withRouter } from 'react-router-dom';
+import Button from './Button';
+import Header from '../components/Header';
+import giphyService, { contentAPI }   from '../services/giphy';
 import GiphyContainer from '../components/GiphyContainer';
 
 const initSearch = {
   result: null,
-  search: '',
-  limit: 5,
+  searchString: '',
+  limit: 20,
   offset: 0
 }
 
@@ -16,82 +16,65 @@ class MainLayout extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {...initSearch, limit: this.props.limit}    
+    this.state = {...initSearch}
     this.getContent = this.getContent.bind(this);
   }
 
-  getContent(content, newSearch = this.state.search) {
-    console.log(newSearch)
-    const {offset, limit, result} = this.state;
+  getContent(content, newSearch = this.state.searchString) {
     
-    giphyService.getContent(content, offset, limit, newSearch)
+    this.props.history.push('/search');
+
+    const {offset, limit, result, searchString} = this.state;
+    let newOffset = offset;
+   
+    if ( newSearch !== searchString ) {
+      newOffset = 0;
+    }
+    
+    giphyService.getContent(content, newOffset, limit, newSearch)
       .then((item) => {
-        console.log(item)
-        const newResult = result === null? [...item.value] : [...result, ...item.value];
-        this.setState( {result: newResult, offset: item.offset} )
+        const newResult = (result === null || newSearch !== this.state.searchString)? [...item.value] : [...result, ...item.value];
+        this.setState( {result: newResult, offset: item.newOffset, searchString: newSearch} )
       });
   }
 
   render() {
     const { result } = this.state;
-    console.log(result);
-    if (!result) {
+    const { pathname } = this.props.location;
+
+    if (!result || pathname !== '/search') {
       return (
         <div className="app">
-          <header className="header">      
-            <div className="header__logo">
-              <h1>GIPHY</h1>
-            </div>
-            <ul className="menu header__menu">
-              <li className="menu__item"><Link to="/">Home</Link></li>
-              <li className="menu__item"><Link to="tranding-gifs">Tranding Gifs</Link></li>
-              <li className="menu__item"><Link to="search">Search</Link></li>
-              <li className="menu__item"><Link to="sport">Sport</Link></li>
-              <li className="menu__item"><Link to="stikers">Stikers</Link></li>
-              <li className="">              
-                <Search
-                  //onClick = {(item) => this.goSearchPage(item)}  
-                  onClick = {(item) => this.getContent('search', item)}
-                />              
-              </li>            
-            </ul>
-          </header>
-          <main>          
+          <Header
+            onClick = {(searchString) => this.getContent(contentAPI.SEARCH_GIFS, searchString)}            
+          />
+          <main className="main">
             {this.props.children}
           </main>
         </div>
       )
     } else {
-    return (
-      <div className="app">
-        <header className="header">      
-          <div className="header__logo">
-            <h1>GIPHY</h1>
-          </div>
-          <ul className="menu header__menu">
-            <li className="menu__item"><Link to="/">Home</Link></li>
-            <li className="menu__item"><Link to="tranding-gifs">Tranding Gifs</Link></li>
-            <li className="menu__item"><Link to="search">Search</Link></li>
-            <li className="menu__item"><Link to="sport">Sport</Link></li>
-            <li className="menu__item"><Link to="stikers">Stikers</Link></li>
-            <li className="">              
-              <Search
-                //onClick = {(item) => this.goSearchPage(item)}  
-                onClick = {(item) => this.getContent('search', item)}
-              />              
-            </li>            
-          </ul>
-        </header>
-        <main>       
-          hello   
-          <GiphyContainer
-            result = {result}            
-          />     
-        </main>
-      </div>
-    )
+      return (
+        <div className="app">
+          <Header 
+            onClick = {(searchString) => this.getContent(contentAPI.SEARCH_GIFS, searchString)}            
+          />
+          <main className="main">
+            <GiphyContainer
+              result = {result}
+            />
+            {result.length > 10 &&
+            <Button
+              className = "button"
+              value = "Load more"
+              onClick = {() => this.getContent(contentAPI.SEARCH_GIFS)}
+            />
+            }
+          </main>
+        </div>
+      )
+    }
   }
-}
 }
 
 export default withRouter(MainLayout);
