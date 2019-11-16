ef4e788f000c7a475949368efc57ee96b6ec2edf
int <yuskovss@gmail.com>
2019-11-16 16:31:28 +0400

    reafactor: remove spases

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,27 @@
 import React, {Component} from 'react';
+import '../scss/search.scss';
 
-export default class Search extends Component {  
+export default class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
       search: '',
     };
 
-    this.handleChange = this.handleChange.bind(this);    
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(event) {    
+  handleChange(event) {
     this.setState( {search: event.target.value} );
   }
 
-  render() {        
+  render() {
     return(
-      <form className="search" onSubmit={(event) => {this.props.onClick(this.state.search);event.preventDefault();}}>
+      <form 
+        className = "search" 
+        onSubmit = {(event) => {
+          this.props.onClick(this.state.search);
+          event.preventDefault();}}>
         <input
           className = "search__input"
           type = "text"
@@ -24,12 +29,10 @@ export default class Search extends Component {
         />
         <input
           className = "search__button"
-          type = "submit"          
+          type = "submit"
           value = "Search"
         />
-        
       </form>
     )
-
   }
 }
