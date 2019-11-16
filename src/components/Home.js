01300ee3f907bfd4e1611eae6daeda40adcd0d30
int <yuskovss@gmail.com>
2019-11-16 16:25:36 +0400

    feat: add new pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import TrendingGifs from './TrendingGifs';
-import SearchGifs from './SearchGifs';
+import ContentContainer from './ContentContainer';
+import { contentAPI } from '../services/giphy';
 
 export default function Home(props) {
   return (
-    <div className = "contentContainer">
-      <TrendingGifs        
-        limit = "5"
-        page = "home"        
-      />      
+    <div className = "main-wrapper">
+      <ContentContainer 
+        title = 'Trending gifs'
+        limit = '5'
+        content = { contentAPI.TRENDING_GIFS }
+        searchString = ''
+      />
+      <ContentContainer
+        title = 'Animals gifs'
+        limit = '5'
+        content = { contentAPI.SEARCH_GIFS }
+        searchString = 'animals'
+      />
+      <ContentContainer
+        title = 'Memes gifs'
+        limit = '5'
+        content = { contentAPI.SEARCH_GIFS }
+        searchString = 'memes'
+      />
+      <ContentContainer
+        title = 'Sport gifs'
+        limit = '5'
+        content = { contentAPI.SEARCH_GIFS }
+        searchString = 'sport'
+      />
+      <ContentContainer
+        title = 'Trending stikers'
+        limit = '5'
+        content = { contentAPI.TRENDING_STIKERS }
+        searchString = ''
+      />
     </div>
   );
 }
--- src/router.js
+++ src/router.js
@@ -1,21 +1,68 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {contentAPI} from './services/giphy';
 
-//layouts
 import MainLayout from './components/MainLayout'
-
-//pages
 import Home from './components/Home';
-import TrendingGifs from './components/TrendingGifs';
-import SearchGifs from './components/SearchGifs';
+import ContentContainer from './components/ContentContainer';
 
 export default(
   <BrowserRouter>
     <MainLayout>
       <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route path="/tranding-gifs" render={ (props) => (<TrendingGifs limit='20' page='trend'/>)} />
-        <Route path="/search" render={ (props) => (<SearchGifs {...props} limit='20' page='search'/>) } />
+        <Route
+          exact path = "/"
+          component = { Home } 
+        />
+        <Route
+          path = "/tranding"
+          render = { (props) => (
+            <ContentContainer 
+              title = "Trending gifs"
+              limit = "20"
+              content = { contentAPI.TRENDING_GIFS }
+              searchString = ""
+            />)}
+        />
+        <Route
+          path = "/animals"
+          render = { (props) => (
+            <ContentContainer title = "Animals gifs"
+              limit = "20"
+              content = { contentAPI.SEARCH_GIFS }
+              searchString = "animals"
+            />)}
+        />
+        <Route
+          path = "/memes"
+          render = { (props) => (
+            <ContentContainer
+              title = "Memes gifs"
+              limit = "20"
+              content = { contentAPI.SEARCH_GIFS }
+              searchString = "memes"
+            />)}
+        />
+        <Route
+          path = "/sport"
+          render = { (props) => (
+            <ContentContainer
+              title = "Sport gifs"
+              limit = "20"
+              content = { contentAPI.SEARCH_GIFS }
+              searchString = "sport"
+            />)}
+        />
+        <Route
+          path = "/stikers"
+          render = { (props) => (
+            <ContentContainer
+              title = "Trending stikers"
+              limit = "20"
+              content = { contentAPI.TRENDING_STIKERS }
+              searchString = ""
+            />)}
+        />        
       </Switch>
     </MainLayout>
   </BrowserRouter>
