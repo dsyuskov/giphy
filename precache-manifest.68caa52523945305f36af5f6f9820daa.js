a5cfde1776fd9c1023e7b5ab521867cbc3447f70
int <yuskovss@gmail.com>
2019-11-16 20:55:33 +0400

    Updates

---
+++ precache-manifest.68caa52523945305f36af5f6f9820daa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de219bbfcf7b9b7d2668c06688499aa",
+    "url": "/giphy/index.html"
+  },
+  {
+    "revision": "2639eb715639022331bb",
+    "url": "/giphy/static/css/main.ef9559df.chunk.css"
+  },
+  {
+    "revision": "63a0733a67243fdfa7cf",
+    "url": "/giphy/static/js/2.d817d117.chunk.js"
+  },
+  {
+    "revision": "2639eb715639022331bb",
+    "url": "/giphy/static/js/main.3eaa1ab3.chunk.js"
+  },
+  {
+    "revision": "9e877a7ea3db8666327e",
+    "url": "/giphy/static/js/runtime-main.51d056ad.js"
+  }
+]);
